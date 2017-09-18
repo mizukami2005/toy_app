e44d27d240d3aea1f5fc4c55a1d35a952f1ece1e
Takamasa Mizukami <mizukami2005@gmail.com>
2017-09-18 20:04:22 +0900

    :heavy_plus_sign: add age

---
+++ db/migrate/20170918110327_add_column_age_to_users.rb
@@ -0,0 +1,5 @@
+class AddColumnAgeToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :age, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124122025) do
+ActiveRecord::Schema.define(version: 20170918110327) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170124122025) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
