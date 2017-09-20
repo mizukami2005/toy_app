25de663a77a799ba35511123147250b360c56f6e
Takamasa Mizukami <mizukami2005@gmail.com>
2017-09-20 20:25:17 +0900

    マイグレーションファイル整理

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918110632) do
+ActiveRecord::Schema.define(version: 20170919074149) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
