1df96472c6dcb62ba1012b3d7b045eb7b30c82ab
Takamasa Mizukami <mizukami2005@gmail.com>
2017-09-20 20:54:07 +0900

    :pencil2: fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920114424) do
+ActiveRecord::Schema.define(version: 20170920115233) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
